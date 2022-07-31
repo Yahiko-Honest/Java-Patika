0cb77fad8ffae55ae632a6b1c3f222862cb7b8be
Yahiko-Honest <101574659+Yahiko-Honest@users.noreply.github.com>
2022-07-31 18:12:26 +0300

    first commit

---
+++ test/src/Giris/Kemaletin.java
@@ -0,0 +1,9 @@
+package Giris;
+
+public class Kemaletin {
+    public static void main(String[] args) {
+        String x = "hey";
+        String y = " naber";
+        System.out.println(x + y);
+    }
+}
